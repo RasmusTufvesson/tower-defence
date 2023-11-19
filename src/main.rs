2544aea562ec0e219e691a552503bcacfd431317
RasmusTufvesson <lars@bizzbook.com>
2023-11-19 23:29:49 +0100

    add spawing and making templates from files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::fs;
+
 use bevy::prelude::*;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 
 fn main() {
     App::new()
@@ -16,21 +18,22 @@ fn main() {
 
 #[derive(Resource, Deserialize)]
 struct TowerTemplate {
-    sprite: &str,
+    sprite: String,
     attack_speed: f32,
-    attack: &str,
+    attack: String,
 }
 
 #[derive(Resource, Deserialize)]
 struct EnemyTemplate {
-    sprite: &str,
+    sprite: String,
     health: f32,
     speed: f32,
+    damage: f32,
 }
 
 #[derive(Resource, Deserialize)]
 struct AttackTemplate {
-    sprite: &str,
+    sprite: String,
     damage: f32,
 }
 
@@ -44,10 +47,47 @@ struct Tower {
 struct Enemy {
     health: f32,
     speed: f32,
+    damage: f32,
+}
+
+fn spawn_tower(commands: &mut Commands, asset_server: &Res<AssetServer>, template: TowerTemplate, transform: Transform) {
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load(&template.sprite),
+            transform: transform,
+            ..default()
+        },
+        Tower {
+            attack: ron::from_str(&fs::read_to_string("assets/".to_string()+&template.attack).unwrap()).unwrap(), attack_speed: template.attack_speed
+        },
+    ));
+}
+
+fn spawn_enemy(commands: &mut Commands, asset_server: &Res<AssetServer>, template: EnemyTemplate, transform: Transform) {
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load(&template.sprite),
+            transform: transform,
+            ..default()
+        },
+        Enemy {
+            health: template.health, speed: template.speed, damage: template.damage
+        },
+    ));
+}
+
+fn get_enemy_template(path: &str) -> EnemyTemplate {
+    ron::from_str(&fs::read_to_string("assets/".to_string()+path).unwrap()).unwrap()
+}
+
+fn get_tower_template(path: &str) -> TowerTemplate {
+    ron::from_str(&fs::read_to_string("assets/".to_string()+path).unwrap()).unwrap()
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2dBundle::default());
+    let enemy = get_enemy_template("enemies/basic.ron");
+    spawn_enemy(&mut commands, &asset_server, enemy, Transform { translation: Vec3 { x: 100.0, y: 100.0, z: 0.0 }, scale: Vec3 { x: 10.0, y: 10.0, z: 1.0}, ..default() })
     // commands.spawn((
     //     SpriteBundle {
     //         texture: asset_server.load("pixel/bevy_pixel_dark.png"),
